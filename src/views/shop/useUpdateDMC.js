797f51e4f7dcfdea8009b03ba684ee2ee332df65
gongrucheng <57404177@qq.com>
2023-11-02 15:46:35 +0800

    no message

--- src/views/shop/useUpdateDMC.js
+++ src/views/shop/useUpdateDMC.js
@@ -59,7 +59,6 @@ export default function useUpdateDMC() {
               onOk,
             });
           } else if (res2.result.approved && !res2.result.refuse) {
-            console.log(111111111111);
             curStepIndex.value = 3;
             userStore.setCloudCodeIsBind(true);
             ambRefuse.value = false;
@@ -148,17 +147,12 @@ export default function useUpdateDMC() {
       let total = res?.result?.total;
       if (total > 0) {
         curStepIndex.value = 4;
+      }else{
+        curStepIndex.value = 3;
       }
-      console.log(curStepIndex.value, 'sxzcz');
     });
   }
-  watch(
-    curStepIndex,
-    (val) => {
-      console.log(val, 'curStepIndexcurStepIndex');
-    },
-    { deep: true },
-  );
+
 
   return {
     getAmbDmc,
