115d58388ec0c0d6146721fe63fccd707219ae16
zk <kai.zhang@megaops.net>
2023-11-23 19:14:50 +0800

    fix: grey poolUrl

--- src/setting.js
+++ src/setting.js
@@ -1,16 +1,13 @@
-
 // setting.js
 export const minSize = 5;
 
-export const shareUrl = "https://share.dev.u2i.net";
-export const s3Url = "devpool.u2i.net";
-export const poolUrl = "devus.u2i.net";
-
+export const shareUrl = 'https://share.dev.u2i.net';
+export const s3Url = 'devpool.u2i.net';
+export const poolUrl = 'devus.u2i.net';
 
 // export const shareUrl = "https://grey.share.fogworks.io";
 // export const s3Url = "s3.grey.vofocorp.com";
-// export const poolUrl = "grey.vofocorp.com";
-
+// export const poolUrl = "us1.vofocorp.com";
 
 // export const shareUrl = "https://share.fogworks.io";
 // export const s3Url = "s3.vofocorp.com";
