8f4c49aa3ecd197a5b305f32769c436d217779f8
xuxin <237461269@qq.com>
2023-12-04 15:46:58 +0800

    fix： app下载上传

--- src/utils/request/ignoreUrl.ts
+++ src/utils/request/ignoreUrl.ts
@@ -1,9 +1,11 @@
+import typeApi from '@/typeApi.js';
+const { apiUrl, shareUrl, chainUrl, nodeUrl, webUrl } = typeApi();
 export default [
-  '/ambmgr/user/check_promo',
-  '/api_accounts/accounts/check_promo',
-  '/order/calc_merkle',
-  '/v1/chain/get_account',
-  '/reCAPTCHA_verification/recaptcha/api/siteverify',
-  '/ambmgr/order/name_set',
-  '/file/valid_upload',
+  `${apiUrl}/ambmgr/user/check_promo`,
+  `${apiUrl}/api_accounts/accounts/check_promo`,
+  `${nodeUrl}/order/calc_merkle`,
+  `${chainUrl}/v1/chain/get_account`,
+  `${apiUrl}/reCAPTCHA_verification/recaptcha/api/siteverify`,
+  `${apiUrl}/ambmgr/order/name_set`,
+  `${nodeUrl}/file/valid_upload`,
 ];
