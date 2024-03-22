3c29a3f1ec60af93b27a7c5f3a5e8265a232367a
rucheng gong <rucheng.gong@megaops.net>
2024-03-22 15:16:28 +0800

    no message

--- src/components/myTransition/index.vue
+++ src/components/myTransition/index.vue
@@ -1,15 +1,12 @@
 <template>
-  <Transition name="fade-transform" mode="out-in" appear>
-    <slot></slot>
-  </Transition>
-
+  <!-- <Transition name="fade-transform" mode="out-in" appear> -->
+  <slot></slot>
+  <!-- </Transition> -->
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
-  
-
   .slide-right-enter-active,
   .slide-right-leave-active {
     transition: transform 0.4s;
@@ -18,12 +15,15 @@
   .slide-right-enter-from {
     transform: translateX(0);
   }
+
   .slide-right-enter-to {
     transform: translateX(0);
   }
+
   .slide-right-leave-from {
     transform: translateX(0);
   }
+
   .slide-right-leave-to {
     transform: translateX(-120%);
   }
@@ -32,23 +32,24 @@
   .fade-transform-leave-active {
     transition: all 0.15s ease;
   }
+
   .fade-transform-enter-active {
     transition: all 0.15s ease;
     transition-delay: 0.1s;
   }
 
   .fade-transform-enter-from {
-    opacity: 0;
     transform: translateX(-30px);
+    opacity: 0;
   }
 
   .fade-transform-enter-to {
+    transform: translateY(0);
     opacity: 1;
-    transform: translateY(0px);
   }
 
   .fade-transform-leave-to {
-    opacity: 0;
     transform: translateX(30px);
+    opacity: 0;
   }
 </style>
